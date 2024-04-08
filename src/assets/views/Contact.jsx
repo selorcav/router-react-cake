23616ab474ddd8305a949f272039238e6525a586
selorcav <se.lorcav@gmail.com>
2024-04-08 00:35:01 -0400

    update readme

--- src/assets/views/Contact.jsx
+++ src/assets/views/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
